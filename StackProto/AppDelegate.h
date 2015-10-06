18298496fdc94b37019b4cc3c13d06577a964d79
Daniel Rakhamimov <danielrakh@gmail.com>
2015-10-06 11:00:36 -0400

    Initial Commit

---
+++ StackProto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackProto
+//
+//  Created by Daniel on 10/6/15.
+//  Copyright © 2015 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackProto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StackProto
+//
+//  Created by Daniel on 10/6/15.
+//  Copyright © 2015 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
